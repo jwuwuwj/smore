8ea11ad016d994483ad8da00cfc86943e12b1e8c
Syce Gaming <77421128+SyceGaming@users.noreply.github.com>
2023-03-27 21:03:07 -0400

    v0.3.3.a.connect-dev

--- backup-receive.js
+++ backup-receive.js
@@ -1,8 +1,10 @@
 window.addEventListener("message", receiveMessage, false);
 
 function receiveMessage(event){
-	if(event.origin !== "https://soft-palmier-05ed85.netlify.app") return;
+	console.log("message received")
+	if(event.origin !== "https://coruscating-shortbread-45f18a.netlify.app") return;
 	if(event.data.id === "sendBackupData"){
+		console.log("confirmed")
 		for(let i = 0; i < event.data.data.length; i++){
 			localStorage.setItem(event.data.data[i].key, event.data.data[i].data)
 		}
